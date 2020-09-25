ecbfa43606f19a9aabaf76b64e2a882d60cc19f2
Yunhyeok Lee <yun.hyeok.lee@gmail.com>
2020-09-24 23:33:14 -0400

    New strategy for Remove Nth Node From End of List.py

--- RemoveNthNodeFromEndofList.py
+++ RemoveNthNodeFromEndofList.py
@@ -5,20 +5,26 @@
 #         self.next = next
 class Solution:
     def removeNthFromEnd(self, head: ListNode, n: int) -> ListNode:
-        fast = slow = head 
+        fast = last = head
+        count = 0
         
-        for i in range(n):
+        while n != count:
             fast = fast.next
+            count += 1
         
         if fast == None:
             return head.next
         
-        while fast.next:
+        while fast.next != None:
             fast = fast.next
-            slow = slow.next # start from first Node 
+            last = last.next
         
-        slow.next = slow.next.next
+        last.next = last.next.next
         
         return head
         
-            
+        # Fast 먼저 N에 도달하게 하고, 그 다음 Fast가 N의 끝이면 리턴하고
+        # 아니면, Slow랑 Fast 동시 출발하면, Fast가 끝에 도달하면,
+        # Slow자리가 N 자리가 됨
+        
+        
