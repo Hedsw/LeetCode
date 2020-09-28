5d3d01e42a3ba3343e73dc93c401fa6835296b31
Yunhyeok Lee <yun.hyeok.lee@gmail.com>
2020-09-28 12:14:12 -0400

    validateBinary Search Tree.py

--- validateBinarySearchTree.py
+++ validateBinarySearchTree.py
@@ -4,38 +4,23 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
-'''           
 class Solution:
     def isValidBST(self, root):
+        if not root:
+            return True
         
         output = []
-        
         self.inorder(root, output)
         
         for i in range(1, len(output)):
             if output[i-1] >= output[i]:
                 return False
         return True
-    
+        
     def inorder(self, root, output):
         if not root:
-            return 
-        
+            return  
         self.inorder(root.left, output)
         output.append(root.val)
         self.inorder(root.right, output)
-'''        
-class Solution:
-    def isValidBST(self, root):
-        return self.validcheck(root, float('-inf'), float('inf'))
-    
-    def validcheck(self, root, left, right):
-        if not root:
-            return True
-        
-        if not left < root.val < right:
-            return False
-        
-        return self.validcheck(root.left, left, root.val) and self.validcheck(root.right, root.val, right)
-        
         
