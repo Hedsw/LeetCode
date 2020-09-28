b0dfd4ef09af533d5821360a9f62a5ba36213fff
Yunhyeok Lee <yun.hyeok.lee@gmail.com>
2020-09-28 11:59:10 -0400

    recursion version and no recursion version

--- maximum_depth_of_binary_tree.py
+++ maximum_depth_of_binary_tree.py
@@ -6,13 +6,13 @@
 #         self.right = right
 class Solution:
     def maxDepth(self, root: TreeNode) -> int:
-        '''
         if not root:
             return 0
         
         return max(self.maxDepth(root.left), self.maxDepth(root.right)) + 1
+        
+        
         '''
-        # This is new version
         if not root:
             return 0
         
@@ -29,3 +29,35 @@ class Solution:
                     queue.append(i.right)
                 array = queue
         return depth
+        '''# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def maxDepth(self, root: TreeNode) -> int:
+        if not root:
+            return 0
+        
+        return max(self.maxDepth(root.left), self.maxDepth(root.right)) + 1
+        
+        # WITHOUT RECURSION
+        '''
+        if not root:
+            return 0
+        
+        array = [root]
+        depth = 0
+        
+        while array:
+            depth += 1
+            queue = []
+            for i in array:
+                if i.left:
+                    queue.append(i.left)
+                if i.right:
+                    queue.append(i.right)
+                array = queue
+        return depth
+        '''
