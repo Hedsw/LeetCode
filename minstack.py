f81e1e6ba26f99351ad1a79137d5d03de4b50141
Yunhyeok Lee <yun.hyeok.lee@gmail.com>
2020-09-30 15:34:43 -0400

    minstack python third solution.py

--- minstack.py
+++ minstack.py
@@ -63,3 +63,37 @@ class MinStack:
         self.minValue = min(self.minValue)
         return self.minValue
 
+
+'''
+Third Solution
+
+'''
+
+class MinStack:
+
+    def __init__(self):
+        """
+        initialize your data structure here.
+        """
+        self.stack = []
+
+    def push(self, x: int) -> None:
+        self.stack.append(x)
+        #print(self.stack)
+        
+    def pop(self) -> None:
+        self.stack.pop()
+
+    def top(self) -> int: 
+        return self.stack[-1]
+    
+    def getMin(self) -> int:
+        return min(self.stack)
+
+
+# Your MinStack object will be instantiated and called as such:
+# obj = MinStack()
+# obj.push(x)
+# obj.pop()
+# param_3 = obj.top()
+# param_4 = obj.getMin()
