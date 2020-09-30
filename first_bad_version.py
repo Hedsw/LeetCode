6672e5bee2272fd8bddd1b7d8de667eb015ef9f9
Yunhyeok Lee <yun.hyeok.lee@gmail.com>
2020-09-29 23:09:20 -0400

    First Bad Version python solution

--- first_bad_version.py
+++ first_bad_version.py
@@ -10,11 +10,10 @@ class Solution:
         
         while low <= high:
             mid = low + (high - low)//2
-            
             if isBadVersion(mid):
                 high = mid - 1
             else:
                 low = mid + 1
+            
         return low
-    
-    
+                
