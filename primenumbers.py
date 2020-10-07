81c39872ed9f867af1319b8f1e3d7349b42eddd1
Yunhyeok Lee <yun.hyeok.lee@gmail.com>
2020-10-07 13:29:47 -0400

    Prime Number Solution Sieve of Eratosthenes

--- primenumbers.py
+++ primenumbers.py
@@ -1,20 +1,21 @@
-# Prime Number 는 2 또는 3으로 나눠지는 숫자를 Prime Number라고 함. 
 class Solution:
     def countPrimes(self, n: int) -> int:
-        if n < 3: return 0
-        lst = [1] * n
-        lst[0], lst[1] = 0, 0
         
-        m = 2
-        while m * m < n:
+        lst = [True]*n
+        
+        if n < 3:
+            return 0 
+        lst[0], lst[1] = 0, 0
+        m = 2 
+        while m*m < n:
             if lst[m]:
-                for i in range(m * m, n , m):
-                    lst[i] = 0
+                for i in range(m*m, n, m):
+                    lst[i] = False
             
             m += 1
-        return sum(lst)
-      
         
+        return sum(lst)
+            
         # Sieve of Eratosthenes를 사용해서 푸는 것 
         
         
