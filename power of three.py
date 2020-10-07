e056ee50787730c18f9f1d76c0918845a320ba57
Yunhyeok Lee <yun.hyeok.lee@gmail.com>
2020-10-07 13:44:32 -0400

    Power of Three Python

--- power of three.py
+++ power of three.py
@@ -1,17 +1,9 @@
 class Solution:
     def isPowerOfThree(self, n: int) -> bool:
         while n > 2:
-            if n % 3 != 0: 
-                return False 
-            n = n / 3
-                
-        return True if n == 1 else False
-    
-    '''
-        while n > 2:
-            if n % 2 != 0;
+            if n % 3 != 0:
                 return False
-            n = n / 2
             
-        return True if n == 1 else False 
-    '''
+            n = n / 3
+            
+        return True if n == 1 else False
