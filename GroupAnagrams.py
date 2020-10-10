7d1c1a8a5fe5fb4b3b8c8c7b1ada58945f2fbe1f
Yunhyeok Lee <yun.hyeok.lee@gmail.com>
2020-10-10 13:14:06 -0400

    Group Anagrams Python

--- GroupAnagrams.py
+++ GroupAnagrams.py
@@ -1,18 +1,15 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        
-        
+              
         # Sort String first
-        # check same string on length
+        # check same string on length        
         ans = collections.defaultdict(list)
-        answer2 = collections.defaultdict(list)
         
         for s in strs:
-            temp2 = tuple(sorted(s))
-            answer2[temp2].append(s)
-        
-        return list(answer2.values())
-    
+            tuples = tuple(sorted(s))
+            ans[tuples].append(s)
+        return list(ans.values())
+            
     # Dictionary를 쓰면 Key Valu로 나뉘게 된다. 
     # 먼저 Sorting을 한 다음에, 그 Sorting한 문자를 기준으로, Key를 설정해두고
     # 각 Value들을 그 Key에 맞게 끼워 넣는다. 
