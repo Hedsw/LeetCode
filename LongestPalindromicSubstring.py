516e2b6794e9bc96246543fde375d043097687ea
Yunhyeok Lee <yun.hyeok.lee@gmail.com>
2020-10-13 00:41:59 -0400

    Logest Palindromic Substrings

---
+++ LongestPalindromicSubstring.py
@@ -0,0 +1,21 @@
+class Solution:
+    def longestPalindrome(self, s: str) -> str:
+        answer = ""
+        for i in range(len(s)):
+            tmp = self.boxs(s, i, i)
+            if len(tmp) > len(answer):
+                answer = tmp
+            
+            tmp = self.boxs(s, i, i+1)
+            if len(tmp) > len(answer):
+                answer = tmp
+        return answer
+            
+    def boxs(self, s, left, right):
+        while left >= 0 and right < len(s) and s[left] == s[right]:
+            left -= 1
+            right += 1
+        return s[left+1:right]
+        
+        
+   
