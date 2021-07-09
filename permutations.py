86972070f302d8feb20121cdf3a77d0c2f87e6fe
Hedsw <yun.hyeok.lee@gmail.com>
2021-07-09 09:44:22 -0400

    Permutation Python

--- permutations.py
+++ permutations.py
@@ -1,17 +1,19 @@
 class Solution(object):
     def permute(self, nums):
-        ans = []
+        ans = []        
         
-        self.helper(nums, [], ans)
+        if len(nums) == 0:
+            return ans
+        
+        self.helper(nums, ans, [])
         return ans
+    
+    def helper(self, nums, ans, tmp):
         
-    def helper(self, nums, tmp, ans):
         if len(nums) == 0:
             ans.append(tmp)
-            return
-        
+            
+            
         for i in range(len(nums)):
-            self.helper(nums[:i] + nums[i+1:], tmp + nums[i], ans)
-        
-
+            self.helper(nums[:i] + nums[i+1:], ans, tmp + [nums[i]])
         
